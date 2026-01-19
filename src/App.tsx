c9a21a911219dd15b512493b0bda533975a98db5
kiruthigaaExaltt <kiruthigaaraju@exalttcoresolutions.com>
2026-01-19 12:38:08 +0530

    yugam

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "../src/context/ThemeContext";
 import BaseLayout from "./layouts/BaseLayout";
 import LoginPage from "./components/login/LoginPage";
-import Footer from "./components/footer/Footer";
-import IndexDashboard from "./components/dashboard/Admin/IndexDashboard";
-import IndexCRM from "./components/CRM/IndexCRM";
-import IndexSettings from "./components/settings/IndexSettings";
-import IndexAccounts from "./components/accounts/IndexAccounts";
+
 
 function App() {
   const isAuthenticated = !!localStorage.getItem("token");
@@ -23,11 +19,6 @@ function App() {
             isAuthenticated ? <BaseLayout /> : <Navigate to="/login" replace />
           }
         >
-          <Route path="/dashboard" element={<IndexDashboard />} />
-          <Route path="/crm" element={<IndexCRM />} />
-          <Route path="/accounts" element={<IndexAccounts />} />
-          <Route path="/settings" element={<IndexSettings />} />
-          <Route path="/footer" element={<Footer />} />
         </Route>
 
         {/* Default redirect */}
